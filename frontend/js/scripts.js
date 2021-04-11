a272bb69565b9c1f73fe9f4e147754b0b8f761e2
Christopher-Schultze <schultzeac@gmail.com>
2021-04-11 17:17:07 -0600

    Added login and signup pages

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -70,4 +70,9 @@ $(function () {
         $('.color-switcher ul li').removeClass('active');
         $(this).addClass('active');
     });
+
+    $(".dropdown-menu li a").click(function(){
+        $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+        $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+      });
 });
