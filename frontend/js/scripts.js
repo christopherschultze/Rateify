f3529e2fd73f0a48b131c4a33fd7c0c91215e81e
Christopher-Schultze <schultzeac@gmail.com>
2021-04-11 22:58:54 -0600

    added template pages for listener, listenerPlaylists, listenerSongs, artist, producer, admin

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -75,4 +75,37 @@ $(function () {
         $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
         $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
       });
+
+      var grid = new ej.grids.Grid({
+        dataSource: data,
+        columns: [
+                 { field: 'OrderID', headerText: 'Order ID', width: 120, textAlign: 'Right' },
+                 { field: 'EmployeeID', headerText: 'Employee ID', width: 150, textAlign: 'Right' },
+                 { field: 'Freight', width: 120, format: 'C2', textAlign: 'Right' },
+                 { field: 'ShipCountry', headerText: 'Ship Country', width: 150 }
+             ]
+     });
+     grid.appendTo('#Grid');
+      
+     document.getElementById("Gridform").addEventListener("submit", (e) => {
+     e.preventDefault();
+       var value = parseInt(document.getElementById('multiplier').value, 10);
+      
+      // Filtering the data with user input value
+       data = new ej.data.DataManager(window.hierarchyOrderdata).executeLocal(new ej.data.Query().where("EmployeeID", "equal", value).take(15));
+      
+       // Assigning to DataGrid
+       grid.dataSource = data;
+      
+       document.getElementById("userinput").style.display = "none";
+       document.getElementById("mtable").style.display = "";
+       document.getElementById("Gridform").reset();
+     });
+     document.getElementById("close").addEventListener("click", (e)=>{
+     document.getElementById("mtable").style.display = "none";
+     document.getElementById("userinput").style.display = "";
+     });
+
+
+      
 });
