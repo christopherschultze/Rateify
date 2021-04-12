b4293a0d127c8eef88bf1e60f57c868b419069fa
Ayubabe <chaudhari.ayush2001@gmail.com>
2021-04-12 17:41:04 -0600

    Update loginConnection.php

--- APIs/loginConnection.php
+++ APIs/loginConnection.php
@@ -4,11 +4,31 @@
     include 'logic.php';    
 
     $conn = connect();
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    $result = login($conn,$username,$password);
-
+    if(isset($_SESSION['username'] && isset($_SESSION['password']))){
+        $username = $_SESSION['username'];
+        $password = $_SESSION['password'];
+    
+        $result = login($conn,$username,$password);
+        $row = mysqli_fetch_assoc($result);
+        if($row['account_type'] == "user"){
+            header("Location: listener.php");
+            die;
+        }
+        else if($row['account_type'] == "artist"){
+            header("Location: artist.php");
+            die;
+        }
+        else if($row['account_type'] == "admin"){
+            header("Location: admin.php");
+            die;
+        }
+        else if($row['account_type'] == "producer"){
+            header("Location: producer.php");
+            die;
+        }
+        return $row;
+    }
+    
     closeCon($conn); 
 
 
