62735066780becd92313746f21c1251da0aa8458
Ayubabe <chaudhari.ayush2001@gmail.com>
2021-04-12 18:05:54 -0600

    Update loginConnection.php

--- APIs/loginConnection.php
+++ APIs/loginConnection.php
@@ -4,32 +4,35 @@
     include 'logic.php';    
 
     $conn = connect();
-    echo "echooooooooooooooooooooooooooooooooooo";
-    if(isset($_SESSION['username']) && isset($_SESSION['password']))){
-        $username = $_SESSION['username'];
-        $password = $_SESSION['password'];
-    
-        $result = login($conn,$username,$password);
-        $row = mysqli_fetch_assoc($result);
-        if($row['account_type'] == "user"){
-            header("Location: listener.php");
-            die;
-        }
-        else if($row['account_type'] == "artist"){
-            header("Location: artist.php");
-            die;
-        }
-        else if($row['account_type'] == "admin"){
-            header("Location: admin.php");
-            die;
-        }
-        else if($row['account_type'] == "producer"){
-            header("Location: producer.php");
-            die;
-        }
-        return $row;
+    if(empty($username) || empty($password)){
+        header("Location: login.php");
+        die;
     }
-    
+    else{
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        
+        $result = login($conn,$username,$password);
+            $row = mysqli_fetch_assoc($result);
+            if($row['account_type'] == "user"){
+                header("Location: listener.php");
+                die;
+            }
+            else if($row['account_type'] == "artist"){
+                header("Location: artist.php");
+                die;
+            }
+            else if($row['account_type'] == "admin"){
+                header("Location: admin.php");
+                die;
+            }
+            else if($row['account_type'] == "producer"){
+                header("Location: producer.php");
+                die;
+            }
+            return $row;
+    }
+
     closeCon($conn); 
 
 
