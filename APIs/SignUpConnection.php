568a435b9648156271e9217bfe0a5c26d44503ad
Ayubabe <chaudhari.ayush2001@gmail.com>
2021-04-13 14:20:22 -0600

    Update SignUpConnection.php

--- APIs/SignUpConnection.php
+++ APIs/SignUpConnection.php
@@ -6,10 +6,12 @@
     $conn = connect();
     $username = $_POST['username'];
     $password = $_POST['password'];
-    // $account_type = $_POST['account_type'];
+    $account_type = $_POST['account_type'];
 
     if(!empty($username) && !empty($password)){
-        signup($conn,$username,$password,'user');
+        signup($conn,$username,$password,$account_type);
+        header("Location: ../frontend/login.php")
+        die;
     }
 
     closeCon($conn); 
