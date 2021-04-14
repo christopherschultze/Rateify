79b4d9542c0b4da51e635290ef39c373536f5ec4
Ayubabe <chaudhari.ayush2001@gmail.com>
2021-04-14 12:09:39 -0600

    Create UpdatePlaysConnection.php

---
+++ APIs/UpdatePlaysConnection.php
@@ -0,0 +1,39 @@
+<?php
+    session_start();
+    include 'connection.php';
+    include 'logic.php';    
+    $conn = connect();
+    $_SESSION['curr_playlist'] = key($_POST['clicked']);
+    $result = searchSongsInPlaylist($conn, $_SESSION['users_playlists'][$_SESSION['curr_playlist']]);
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            $songInfo = searchSong($conn, $row["song_id"]);
+            if ($result->num_rows > 0){
+                while($row2 = $songInfo->fetch_assoc())
+                {
+                    $song_info[] = $row2;
+                    // $row2["name"]; //song name
+                    // $row2["id"]; // song id
+                    // $row2['album_name']; //album name
+                    // $row2['no_of_plays']; //album name
+                    // $row2['duration']; //album name
+                    // $row2['date_created']; //album name
+                }
+            }
+        }
+
+        $_SESSION['songs_info'] = $song_info;
+      } 
+      else{
+          $_SESSION['songs_info'] = NULL;
+      }
+    //   echo $_SESSION['songs_info'][0]['name'];
+
+    header("Location: ../frontend/listenerPlaylists.php");
+
+
+    closeCon($conn); 
+
+
+?>
