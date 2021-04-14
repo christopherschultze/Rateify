951db195e2c3edc2b57c1702aaa300d33505828e
Ayubabe <chaudhari.ayush2001@gmail.com>
2021-04-13 21:14:09 -0600

    Update DisplayPlaylistsConnection.php

--- APIs/DisplayPlaylistsConnection.php
+++ APIs/DisplayPlaylistsConnection.php
@@ -6,7 +6,7 @@
     $conn = connect();
     $username = $_SESSION['username'];
     
-
+    $_SESSION['curr_playlist'];
     $result = searchPlaylistsByUser($conn,$username);
 
         if ($result->num_rows > 0) {
@@ -16,7 +16,30 @@
 
             $_SESSION['users_playlists'] = $playlist_names;
         }
-            header("Location: ../frontend/listenerPlaylists.php");
+
+        $result = searchSongsInPlaylist($conn, $_SESSION['users_playlists'][$_SESSION['curr_playlist']]);
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                $songInfo = searchSong($conn, $row["song_id"]);
+                if ($result->num_rows > 0){
+                    while($row2 = $songInfo->fetch_assoc())
+                    {
+                        $song_info[] = $row2;
+                        // $row2["name"]; //song name
+                        // $row2["id"]; // song id
+                        // $row2['album_name']; //album name
+                        // $row2['no_of_plays']; //album name
+                        // $row2['duration']; //album name
+                        // $row2['date_created']; //album name
+                    }
+                }
+            }
+    
+            $_SESSION['songs_info'] = $song_info;
+          } 
+
+        header("Location: ../frontend/listenerPlaylists.php");
 
     closeCon($conn); 
 
