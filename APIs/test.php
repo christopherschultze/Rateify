b78ccdbafe75f5d0872f03cef9a0c4ad4fe33731
Ayubabe <chaudhari.ayush2001@gmail.com>
2021-04-14 14:16:01 -0600

    Update test.php

--- APIs/test.php
+++ APIs/test.php
@@ -6,12 +6,12 @@
     // createPlaylist($conn, 'playlist1', 'user1');
     // $result = searchSongsInAlbum($conn, 'album1');
     // addRating($conn, 'user1', 1, 'this shit is lit', 500);
-    $result = searchSong($conn, 0);
+    $result = searchPlaylistsByUser($conn, 'user1');
 
     if ($result->num_rows > 0) {
       // output data of each row
       while($row = $result->fetch_assoc()) {
-         echo $row['name'];
+         echo $row['playlist_name'];
          echo $row['user_username'];
         }
     } else {
