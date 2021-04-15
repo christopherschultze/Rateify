205a6a70240d4caff2f02908e49b2840069999d2
Ayubabe <chaudhari.ayush2001@gmail.com>
2021-04-14 21:40:12 -0600

    Create DisplayArtistSongsConnection.php

---
+++ APIs/DisplayArtistSongsConnection.php
@@ -0,0 +1,32 @@
+<?php
+    session_start();
+    include 'connection.php';
+    include 'logic.php';    
+
+    $conn = connect();
+    $username = $_SESSION['username'];
+    // $artists = array();
+    $result = searchSongByArtist($conn,$username);
+
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                $songInfo = searchSong($conn, $row['song_id']);
+                if ($result->num_rows > 0){
+                    while($row2 = $songInfo->fetch_assoc())
+                    {
+                        $artists_song_info[] = $row2;
+                    }
+                }
+            }
+            $_SESSION['artists_songs'] = $artists_song_info;
+        }else{
+            $_SESSION['artists_songs'] = NULL;
+        }
+        
+        //echo $_SESSION['artists_songs']['name'];
+        header("Location: ../frontend/ArtistViewSongs.php");
+
+    closeCon($conn); 
+
+
+?>
