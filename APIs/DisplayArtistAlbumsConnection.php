4c246c6c56a700cf94685518f3064d34039759b3
Ayubabe <chaudhari.ayush2001@gmail.com>
2021-04-14 22:30:04 -0600

    Update DisplayArtistAlbumsConnection.php

--- APIs/DisplayArtistAlbumsConnection.php
+++ APIs/DisplayArtistAlbumsConnection.php
@@ -7,12 +7,13 @@
     $username = $_SESSION['username'];
     $result = searchArtistAlbum($conn,$username);
 
-        if ($result->num_rows > 0) {
+        if ($result != false && $result->num_rows > 0) {
             while($row = $result->fetch_assoc()) {
                 $albumInfo = searchAlbum($conn, $row['album_name']);
-                if ($result->num_rows > 0){
-                    while($row2 = $songInfo->fetch_assoc())
+                if ($albumInfo->num_rows > 0){
+                    while($row2 = $albumInfo->fetch_assoc())
                     {
+                        echo $row2['name'];
                         $artists_album_info[] = $row2;
                     }
                 }
@@ -22,7 +23,7 @@
             $_SESSION['artists_albums'] = NULL;
         }
         
-       // header("Location: ../frontend/ArtistViewAlbums.php");
+        header("Location: ../frontend/ArtistViewAlbums.php");
 
     closeCon($conn); 
 
