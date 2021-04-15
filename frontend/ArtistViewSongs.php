991ff661ba8315803891facb0a032dc12eac7a20
Ayubabe <chaudhari.ayush2001@gmail.com>
2021-04-14 20:57:33 -0600

    Create ArtistViewSongs.php

---
+++ frontend/ArtistViewSongs.php
@@ -0,0 +1,86 @@
+<?php
+  session_start();
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Rateify - Artist</title>
+    <meta name="description"
+          content="Rateify is a music service that allows users to rate songs"/>
+
+    <!--Inter UI font-->
+    <link href="https://rsms.me/inter/inter-ui.css" rel="stylesheet">
+
+    <!--vendors styles-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css">
+
+    <!-- Bootstrap CSS / Color Scheme -->
+    <link rel="stylesheet" href="css/default.css" id="theme-color">
+</head>
+<body>
+
+<!--navigation-->
+<section class="smart-scroll">
+    <div class="container-fluid">
+        <nav class="navbar navbar-expand-md navbar-dark">
+            <a class="navbar-brand heading-black" href="index.php">
+                Rateify
+            </a>
+            <button class="navbar-toggler navbar-toggler-right border-0" type="button" data-toggle="collapse"
+                    data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                <span data-feather="grid"></span>
+            </button>
+            
+        </nav>
+    </div>
+</section>
+
+<!-- listener functionality -->
+<section class="py-7 py-md-0 bg-hero" id="login">
+    <div class="container">
+        <div class="row vh-md-100">
+            <div class="col-12 mx-auto my-auto text-center">
+              
+              <div class="col text-center">
+              <h1> View my songs. </h1>
+              </div>
+
+              <!-- hyperlinks -->
+              <div class="col text-center">
+                <a href="artist.php"> Return to action page</a>.
+              </div>
+
+              <!-- view song form -->
+              <form action="#" method="post">
+
+                TODO: table of artists' songs
+                
+              </form>
+              
+            </div>
+        </div>
+    </div>
+</section>
+
+<!--scroll to top-->
+<div class="scroll-top">
+    <i class="fa fa-angle-up" aria-hidden="true"></i>
+</div>
+
+
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.7.3/feather.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+<script src="js/scripts.js"></script>
+</body>
+</html>
