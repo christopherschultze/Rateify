bf388b2986b7705bbc2a8035efb7814fea3a8dfe
Ayubabe <chaudhari.ayush2001@gmail.com>
2021-04-15 13:08:03 -0600

    Update UpdatePlaysConnection.php

--- APIs/UpdatePlaysConnection.php
+++ APIs/UpdatePlaysConnection.php
@@ -4,18 +4,30 @@
     include 'logic.php';    
     $conn = connect();
     $temp = $_SESSION['temp'];
-    $_SESSION['song_playing'] = $_POST[$temp];
-    $_SESSION['temp']++;
-    echo $_SESSION['temp'];
+    $_SESSION['song_playing'] = key($_POST['song_played']);
     
-    // echo $_SESSION['random'];
-    //   echo $_SESSION['songs_info'][0]['name'];
+    increaseNoOfPlays($conn,$_SESSION['song_playing']);
 
-    // header("Location: ../frontend/listenerPlaylists.php");
-    function hello($message)
-    {
-        echo $message;
-    }
+    $result = searchSongsInPlaylist($conn, $_SESSION['users_playlists'][$_SESSION['curr_playlist']]);
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $songInfo = searchSong($conn, $row["song_id"]);
+            if ($result->num_rows > 0){
+                while($row2 = $songInfo->fetch_assoc())
+                {
+                    $song_info[] = $row2;
+                }
+            }
+        }
+
+        $_SESSION['songs_info'] = $song_info;
+      } 
+      else{
+          $_SESSION['songs_info'] = NULL;
+      }
+
+    header("Location: ../frontend/listenerPlaylists.php");
+    header("Location: ../frontend/listenerPlaylists.php");
 
     closeCon($conn); 
 
