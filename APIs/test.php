779d04ed36b2c498770201d02c437d28a16ee788
Ayubabe <chaudhari.ayush2001@gmail.com>
2021-04-14 22:30:07 -0600

    Update test.php

--- APIs/test.php
+++ APIs/test.php
@@ -3,72 +3,15 @@
     include 'logic.php';
     
     $conn = connect();
-    // createPlaylist($conn, 'playlist1', 'user1');
-    // $result = searchSongsInAlbum($conn, 'album1');
-    // addRating($conn, 'user1', 1, 'this shit is lit', 500);
-    $result = searchSongByName($conn, 'SICKO MODE');
-    $ids = array();
-    $artists = array();
-    $album_names = array();
+    $result = searchArtistAlbum($conn,'artist');
     if ($result->num_rows > 0) {
       // output data of each row
       while($row = $result->fetch_assoc()) {
-          array_push($ids, $row['id']);
-          array_push($album_names, $row['album_name']);
+          echo $row['album_name'];
         }
       } else {
         echo "0 results";
       }
-      foreach($ids as &$id)
-      {
-        $artist = searchArtistBySong($conn, $id);
-        echo "for song id: " .$id. "<br>";
-        echo "Artists are: "; 
-        if($artist -> num_rows > 0)
-          {
-            while($row2 = $artist->fetch_assoc())
-            {
-                echo $row2['artist_username'];
-                // array_push($artists, $row2['artist_username']);
-            }
-            echo "<br>";
-          }
-      }
-            // print_r($ids);
-      // echo "Info for SICKO MODE: ";
-      // echo "<br>";
-      // echo "album name: ";
-      // foreach($album_names as &$album)
-      // {
-      //   echo $album;
-      //   echo " ";
-      // }
-      // echo "<br>";
-      // echo "artist name: ";
-      // foreach($artists as &$artist)
-      // {
-      //   echo $artist;
-      //   echo " ";
-      // }
-    // echo $result;
-    // if ($result->num_rows > 0) {
-    //     // output data of each row
-    //     while($row = $result->fetch_assoc()) {
-    //         echo "song id: " . $row["id"]. "<br>";
-    //     }
-    //   } else {
-    //     echo "0 results";
-    //   }
-    // header("Content-type: JSON");
-    // $output = array();
-    // $row_number = 0;
-    // // while($row = mysqli_fetch_array($result))
-    // $row = mysqli_fetch_array($result)
-    // // {
-    // $output[$row_number]['id'] = $row['id'];
-    //     // $row_number++;
-    // // }
-
-    // echo json_encode($output, JSON_PRETTY_PRINT);
+      
     closeCon($conn);
 ?>
