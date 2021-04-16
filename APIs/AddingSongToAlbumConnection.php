1e0aff8c6c06800d1225c6e3ca7360f91ac2350b
Ayubabe <chaudhari.ayush2001@gmail.com>
2021-04-15 23:27:35 -0600

    Create AddingSongToAlbumConnection.php

---
+++ APIs/AddingSongToAlbumConnection.php
@@ -0,0 +1,46 @@
+<?php
+    session_start();
+    include 'connection.php';
+    include 'logic.php';    
+
+    $conn = connect();
+    $_SESSION['song_add_album'] = key($_POST['song_adding_album']);
+
+    addSongToAlbum($conn,$_SESSION['selected_album']['name'],$_SESSION['song_add_album']);
+
+    $username = $_SESSION['username'];
+    $result = searchSongsInAlbum($conn,$_SESSION['selected_album']['name']);
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $result2 = searchSong($conn,$row['song_id']);
+            if ($result2->num_rows > 0) {
+                while($row2 = $result2->fetch_assoc()) {
+                        $song_info[] = $row2;
+                }
+            }
+        }
+        $_SESSION['album_songs'] = $song_info;
+    }
+    else
+    {
+        $_SESSION['album_songs'] = NULL;
+    }
+
+    $result3 = searchAlbum($conn, $_SESSION['album']);
+    if ($result3->num_rows > 0) {
+        while($row3 = $result3->fetch_assoc()) {
+            $album_info = $row3;
+        }
+
+        $_SESSION["selected_album"] = $album_info;
+    }
+    else{
+        $_SESSION["selected_album"] = NULL;
+    }
+   
+    header("Location: ../frontend/AlbumPage.php");
+
+    closeCon($conn); 
+
+
+?>
