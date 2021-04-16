68ed397e909c956efbfeb367883b199e9e309433
Ayubabe <chaudhari.ayush2001@gmail.com>
2021-04-16 01:24:13 -0600

    Update createPlaylistConnection.php

--- APIs/createPlaylistConnection.php
+++ APIs/createPlaylistConnection.php
@@ -3,24 +3,26 @@
     include 'connection.php';
     include 'logic.php';
     $conn = connect();
-    // echo "<script>alert('Success!');</script>";
     $playlist_name = $_POST['playlist_name'];
     $username = $_SESSION['username'];
-    // echo $username;
-    // $result = searchPlaylistsByUser($conn, $username);
-    // echo $result->num_rows;
-    // if($result->num_rows == 0)
-    // {
         createPlaylist($conn, $playlist_name, $username);
-        // $_SESSION['playlist_success'] = TRUE;
-        // echo "added";
-    // }
-    // else
-    // {
-        // echo "failed";
-        // header("Location: ../frontend/createPlaylist.php");
-        // $_SESSION['playlist_success'] = FALSE;
-        // echo "failed";
-    // }
+    $result = searchSongsInPlaylist($conn, $_SESSION['users_playlists'][$_SESSION['curr_playlist']]);
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $songInfo = searchSong($conn, $row["song_id"]);
+             if ($result->num_rows > 0){
+                 while($row2 = $songInfo->fetch_assoc())
+                {
+                     $song_info[] = $row2;
+                }
+            }
+        }
+    
+            $_SESSION['songs_info'] = $song_info;
+          } 
+          else{
+              $_SESSION['songs_info'] = NULL;
+          }
+        header("Location: ../frontend/listenerPlaylists.php");
     header("Location: ../frontend/createPlaylist.php");
 ?>
